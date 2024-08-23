57b2862ef4493c552752d3108c51507cd0f19510
Louis Fisher <louis.fisher1919@gmail.com>
2024-08-23 18:08:59 +0100

    use new data tables

--- notebooks/andexanet.sql
+++ notebooks/andexanet.sql
@@ -1,17 +1,19 @@
 SELECT 
     scmd_table.year_month,
-    scmd_table.vmp_snomed_code,
-    scmd_table.vmp_product_name,
-    scmd_table.total_quanity_in_vmp_unit,
-    scmd_table.unit_of_measure_name,
+    scmd_table.vmp_code,
+    scmd_table.vmp_name,
+    scmd_table.SCMD_quantity,
+    scmd_table.SCMD_quantity_basis,
+    scmd_table.dose_quantity,
+    scmd_table.dose_unit,
     ods.ods_code AS ods_code, 
     ods.ods_name AS ods_name,
     ods.region AS region,
 FROM 
-    scmd.scmd AS scmd_table
+    scmd.dose AS scmd_table
 LEFT JOIN 
-    scmd.ods AS ods
+    scmd.ods_mapped AS ods
 ON 
     scmd_table.ods_code = ods.ods_code
 WHERE
-    scmd_table.vmp_snomed_code in (37454211000001101)
+    scmd_table.vmp_code in (37454211000001101)
